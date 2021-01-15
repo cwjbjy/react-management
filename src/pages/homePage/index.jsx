508ea682315ebb6f5842df78c5aaac6e8ecd0c49
wj.cao <595842570@qq.com>
2021-01-15 14:08:45 +0800

    人员列表开发

--- src/pages/homePage/index.jsx
+++ src/pages/homePage/index.jsx
@@ -2,17 +2,18 @@ import { Row, Col } from "antd";
 import UserCard from "./components/userCard";
 import ProgressCard from "./components/progressCard";
 import Message from "./components/message";
+import Schedule from "./components/schedule";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as userAction from "../../react-redux/action/userAction";
 import { useEffect, useState } from "react";
+import "./index.scss";
 const HomePage = (props) => {
-
   let imageUrl = props.img.imageUrl;
   let userName = localStorage.getItem("userName");
   let role = userName === "一叶扁舟" ? "管理员" : "普通用户";
 
-  const [registerTime, getTime] = useState('');
+  const [registerTime, getTime] = useState("");
 
   useEffect(() => {
     const params = {
@@ -21,25 +22,29 @@ const HomePage = (props) => {
     props.userAction.getUser(params).then((res) => {
       getTime(res.Data[0].createTime);
     });
-  }, [userName,props.userAction]);
+  }, [userName, props.userAction]);
 
-  
   return (
     <section>
-      <Row>
-        <Col span={8} lg={8} xl={8}>
-          <UserCard
-            imageUrl={imageUrl}
-            userName={userName}
-            registerTime={registerTime}
-            role={role}
-          />
-          <ProgressCard />
-        </Col>
-        <Col span={16} lg={16} xl={16}>
-          <Message />
-        </Col>
-      </Row>
+      <div className="homePage">
+        <Row>
+          <Col span={8} lg={8} xl={8}>
+            <UserCard
+              imageUrl={imageUrl}
+              userName={userName}
+              registerTime={registerTime}
+              role={role}
+            />
+            <ProgressCard />
+          </Col>
+          <Col span={16} lg={16} xl={16}>
+            <Message />
+            <div className="Schedule">
+              <Schedule />
+            </div>
+          </Col>
+        </Row>
+      </div>
     </section>
   );
 };
