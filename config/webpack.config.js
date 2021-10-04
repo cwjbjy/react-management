5bb56d75254119f7aae0493a88534f56b0518f33
wj.cao <595842570@qq.com>
2021-10-04 14:31:59 +0800

    开启gzip压缩

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -26,7 +26,7 @@ const ModuleNotFoundPlugin = require("react-dev-utils/ModuleNotFoundPlugin");
 const ForkTsCheckerWebpackPlugin = require("react-dev-utils/ForkTsCheckerWebpackPlugin");
 const typescriptFormatter = require("react-dev-utils/typescriptFormatter");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
-
+const CompressionPlugin = require("compression-webpack-plugin");
 const postcssNormalize = require("postcss-normalize");
 
 const appPackageJson = require(paths.appPackageJson);
@@ -304,14 +304,14 @@ module.exports = function (webpackEnv) {
       splitChunks: {
         chunks: "all",
         name: false,
-        cacheGroups:{
+        cacheGroups: {
           antd: {
             name: "antd",
             chunks: "all",
             test: /[\\/]node_modules[\\/](antd|@ant-design)[\\/]/,
             priority: 40,
           },
-        }
+        },
       },
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
@@ -617,6 +617,8 @@ module.exports = function (webpackEnv) {
             skipWaiting: true,
           })
         : undefined,
+
+      isEnvProduction && new CompressionPlugin(),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
       // https://github.com/facebook/create-react-app/issues/5358
