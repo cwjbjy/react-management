247bbf87dc632dad1343a33961278e89be9c5936
cwj <595842570@qq.com>
2022-04-18 14:24:13 +0800

    feat: 图片 (story#000)

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-import "antd/dist/antd.css";
-import "./assets/icon/iconfont.css";
+import 'antd/dist/antd.css';
+import './assets/icon/iconfont.css';
 
-import ReactDOM from "react-dom";
-import store from "./store";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import renderRoutes from "./routes/renderRoutes";
-import routeConfig from "./config/routeConfig.js";
+import ReactDOM from 'react-dom';
+import store from './store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import renderRoutes from './routes/renderRoutes';
+import routeConfig from './config/routeConfig.js';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>{renderRoutes(routeConfig)}</BrowserRouter>
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
-if (process.env.NODE_ENV === "production") {
-  if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
+
+if (process.env.NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
       navigator.serviceWorker
-        .register("/service-worker.js")
+        .register('/service-worker.js')
         .then((registration) => {
-          console.log("SW registered: ", registration);
+          console.log('SW registered: ', registration);
         })
         .catch((registrationError) => {
-          console.log("SW registration failed: ", registrationError);
+          console.log('SW registration failed: ', registrationError);
         });
     });
   }
